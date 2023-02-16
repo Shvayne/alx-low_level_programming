0c202e11739dc90786f2e46fc8e1631a5810328d
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-16 11:12:32 -0800

    fixed indentation error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
-
-
 /**
- * main - prints "programming is like 
- * building a multilingual puzzle.
- * followed by a new line
- * Return: Always (0)
+ * main - a program that prints a line with puts function
+ * Return: 0 (success)
  */
-
-
 int main(void)
-
-
-{	
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
-
