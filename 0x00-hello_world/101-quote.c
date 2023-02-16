755d09ef52fc6328babf54ad7a77a31d181cdc91
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-16 13:28:07 -0800

    error returned

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that prints some specified string to the standard error
- * Return: 1 (success)
+ * main - A program that prints some string to stderr
+ * Return: 1 
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
