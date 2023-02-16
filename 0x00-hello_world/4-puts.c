9d46c17d188cf57b948b276b070ac01ed8088b86
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-16 09:10:54 -0800

    getchar to print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+/**
+ * main - prints "programming is like a building a multilingual puzzle, followed by a new line."
+ * return (0)
+ */
+
+int main(void)
+
+{
+         puts("\"Programming is like building a multilingual puzzle");
+
+	 return(0);
+}
+
