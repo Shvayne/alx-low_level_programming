5832fc0dfd22935c7c4c1100315c4478565615ad
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-17 12:49:40 -0800

    prints all digits without printf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all digits 0-9 without
+ * printf
+ * Return: alway 0
+ */
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
