33a0cd70224564b031ebaebe64936511b3a18a66
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-16 23:41:38 -0800

    fixed trailing whitespace

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints some string to stderr
- * Return: 1 
+ * main - prints some quoted strings to standard error
+ * Return: Always 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is  useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
