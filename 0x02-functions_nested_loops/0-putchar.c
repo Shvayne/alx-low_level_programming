11bc2bf90aea74437e7a6c6e992a4bf13aeda190
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-21 07:23:53 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
 #include "main.h"
 /**
  * main - entry point
- * Return: 0 if success
+ * Return: always 0
  */
 int main(void)
 {
-print_putchar();
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar('\n');
 return (0);
 }
--- 0x02-functions_nested_loops/seperate.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "main.h"
-
-void print_putchar()
-{
-printf("_putchar\n");
-}
