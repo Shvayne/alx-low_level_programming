82cebff80e6beb1826bc8b6754a89b7f746ac63c
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-21 08:52:58 -0800

    prints character a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,10 @@
  */
 void print_alphabet(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
+char a;
+for (a = 97; a <= 122; ++a)
 {
-_putchar(letter);
+_putchar(a);
 _putchar('\n');
 }
 return;
