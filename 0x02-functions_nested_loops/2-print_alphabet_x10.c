3e3e3e3ca286b60d23f8fa08ce33a4fc131e315d
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-21 09:41:02 -0800

    print alphabet with loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alpabet
+ * 10 times with loop
+ */
+void print_alphabet_x10(void)
+{
+char a;
+int i;
+int printed = 0;
+for (i = 1; i <= 10; i++)
+{
+for (a = 97; a <= 122; ++a)
+{
+_putchar(a);
+printed = 1;
+}
+if (printed)
+{
+_putchar('\n');
+}
+return;
+}
+}
