257aa6e1aa691d9ecb3d16d81b46efe414aa7694
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-21 17:16:44 -0800

    looping

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 for sucesss
+ */
+int main(void)
+{
+int sum = 0;
+for (int i = 1; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
