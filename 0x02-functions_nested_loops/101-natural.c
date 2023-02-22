e40e81bab20207b503977e280464772dea2f6b37
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-21 17:27:27 -0800

    looping

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,13 +5,11 @@
  */
 int main(void)
 {
-int sum = 0;
-for (int i = 1; i < 1024; i++)
+int i, sum = 0;
+for (int i = 0; i < 1024; i++)
 {
-if (i % 3 == 0 || i % 5 == 0)
-{
+if ((i % 3) == 0 || (i % 5) == 0)
 sum += i;
-}
 }
 printf("%d\n", sum);
 return (0);
