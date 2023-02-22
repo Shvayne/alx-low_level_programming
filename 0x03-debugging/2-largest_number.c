7f7c5131d94efe836e02bb812f944ee3d317a859
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-22 12:13:37 -0800

    print the largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,20 +8,12 @@
  */
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
+int largest = a;
+if (b > largest)
 {
 largest = b;
 }
-else
+if (c > largest)
 {
 largest = c;
 }
