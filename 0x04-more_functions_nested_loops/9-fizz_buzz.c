f9f027714b8d2f885955d5d9c34e58862e8737c6
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-23 08:02:23 -0800

    fizz buzz quiz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 1-100
+ * but prints fizz or buzz for muliples
+ * of 5 or 3
+ * Return: 0
+ */
+int main(void)
+{
+for (int i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else
+	{
+	printf("%d ", i);
+	}
+}
+printf("\n");
+return (0);
+}
