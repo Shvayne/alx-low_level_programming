38a3a3e59293feb76ed41b39b0cbe86896fdaa25
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-23 08:37:19 -0800

    prints a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+int i, j
+if (n < 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+}
