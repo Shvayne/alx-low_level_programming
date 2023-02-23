c1537d4c37541d21f99c1ef502252f7d57ca8dcc
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-23 04:59:55 -0800

    prints 0-9 apart from 0 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,12 @@ void print_most_numbers(void)
 int c;
 for (c = 48; c <= 58; c++)
 {
-if (c != 48 && c != 52)
+if (c != 50)
 {
-_putchar(c + '0');
+if (c != 52)
+{
+_putchar(c);
+}
 }
 }
 _putchar('\n');
