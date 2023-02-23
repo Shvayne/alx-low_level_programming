9013e9c5340516b1d71b4c27353b1ec2a77a35b3
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-23 04:43:27 -0800

    prints 0-9 apart from 0 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_most_numbers - prints umbers 0-9
  * apart from 0 and 4
@@ -11,8 +12,8 @@ for (c = 0; c <= 9; c++)
 {
 if (c != 0 && c != 4)
 {
-_putchar(c + '0');
+putchar(c + '0');
 }
 }
-_putchar('\n');
+putchar('\n');
 }
