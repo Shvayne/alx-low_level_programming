c0439fee43cc7634c1a40a3e9c283e6c545dd592
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-23 04:37:01 -0800

    print int

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_numbers - prints numbers followed by a
  * new line
@@ -9,7 +10,7 @@ void print_numbers(void)
 int i;
 for (i = 48; i < 58; i++)
 {
-_putchar('i');
+_putchar(i);
 }
 _putchar('\n');
 return;
