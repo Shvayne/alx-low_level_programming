633a8f76af8bd7b5abcd3a9177fe87e430f26045
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-24 13:09:29 -0800

    print int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,20 @@
  */
 void print_number(int n)
 {
+	unsigned int n1 = 0;
+
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n = -n;
 	}
-	if (n / 10)
+	else
 	{
-		print_number(n / 10);
+		n1 = n;
+	}
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
 	}
 	_putchar(n % 10 + '0');
 }
