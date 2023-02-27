eef4a09b432e2fed6eaea30a9b17efa4ffa5b7d7
Christiana Apinoko <christyapinoko@gmail.com>
2023-02-27 11:01:12 -0800

    update array

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - point to an array
+ * @n: array
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+here we talk all about pointers and arrays in C
