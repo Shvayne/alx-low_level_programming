8e3370aa44cb6a932d8352539102aa16204fc019
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-01 12:42:44 -0800

    added readme, main.h and strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,13 +6,14 @@
  * _strcat - concancate two strings
  * @dest: destination
  * @src: input value
+ * Return: newstring
  */
 char *_strcat(char *dest, char *src)
 {
 	int len1 = strlen(dest);
 	int len2 = strlen(src);
 
-	char* newstring = (char*) malloc (len1 + len2 + 1);
+	char *newstring = (char *) malloc(len1 + len2 + 1);
 
 	strcpy(newstring, dest);
 	strcat(newstring, src);
