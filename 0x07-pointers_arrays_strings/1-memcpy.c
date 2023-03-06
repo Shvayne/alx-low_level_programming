36d1906c926a9238d627c9bccb30329cc3532cf8
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-06 13:18:14 -0800

    copy memory location to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - copy bytes from one memory
+ * location to another
+ * Return: destination
+ * @dest: destination
+ * @src: copy from
+ * @n: byte count
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dest_startpoint = dest;
+	char *src_startpoint = src;
+
+	while (n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (dest_startpoint);
+}
