82e92aae83e4ef1ce281102b3fbeedc374536661
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-07 12:58:03 -0800

    added, readme, main header and puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string recursively
+ * @s: pointer to character
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+here we get into the world of function calling itself, recursion which can be used to solve complex problems in programming
