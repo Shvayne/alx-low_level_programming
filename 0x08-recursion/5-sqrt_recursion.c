788df855ff6194211a9e698f80866124c9c7b6c0
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-07 15:19:27 -0800

    find square root file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,52 +1,32 @@
 #include "main.h"
-/**
- * _sqrt_recursion_helper - a smaller cunk of the
- *  code
- *  @n: value
- *  @low: value
- *  @high: value
- *  Return: int
- */
-int _sqrt_recursion_helper(int n, int low, int high)
-{
-	if (low > high)
-	{
-		return (-1);
-	}
-	int mid = (low + high) / 2;
-	int square = mid * mid;
-
-	if (square == n)
-	{
-		return (mid);
-	}
-	else if (square < n)
-	{
-		return (_sqrt_recursion_helper(n, mid + 1, high));
-	}
-	else
-	{
-		return (_sqrt_recursion_helper(n, low, mid + 1));
-	}
-}
+int actual_sqrt_recursion(int n, int i);
 
 /**
- * _sqrt_recursion - outputs the square of a value
- * @n: value
- * Return: int
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (_sqrt_recursion_helper(n, 1, n / 2 + 1));
-	}
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
