fe2be323d6a8cadf65ac85aac15374d34e04af33
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-10 15:11:03 -0800

    print argument num using argument to main

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * @argc: argument count
+ * @argv: array holdin argument
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
