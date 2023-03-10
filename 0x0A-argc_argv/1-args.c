03b1cf5ff9aa5b2543f6d087c30c94435d850282
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-10 15:20:58 -0800

    print argument num using argument to main

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
  * @argv: array holdin argument
  * Return: 1 or 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
