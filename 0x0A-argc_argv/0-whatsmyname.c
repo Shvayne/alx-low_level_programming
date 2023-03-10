9717802d945d30322cc5ba36b0006a96a1257cf8
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-10 14:52:18 -0800

    print program name using argument to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print its name
+ * Return: 1 or 0
+ * @argc: argument count
+ * @argv: array
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
