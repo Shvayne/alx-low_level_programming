802dca8427fdd5a5b44bb157ec34fa2c67198f75
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-11 04:01:32 -0800

    a program that accept 1 command line argument

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
- * main - prints coin balance
- * @argc: argument count
- * @argv: argument array
- * Return: 0
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int cent = atoi(argv[1]);
-
-	if (cent < 0)
+	num = atoi(argv[1]);
+	result = 0;
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	int coins[] = {25, 10, 5, 1};
-	int num_coins = sizeof(coins) / sizeof(int);
-	int i;
-	int count = 0;
-
-	for (i = 0; i < num_coins; i++)
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		while (cent >= coins[i])
+		while (num >= coins[j])
 		{
-			cent -= coins[i];
-			count++;
+			result++;
+			num -= coins[j];
 		}
 	}
-	printf("%d\n", count);
+	printf("%d\n", result);
 	return (0);
 }
