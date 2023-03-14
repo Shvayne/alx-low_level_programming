2e1b9a86f0c6d910e298591e331fabb2105f9bd6
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-14 05:08:56 -0700

    fixed CSO 90

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *arr = malloc(size * sizeof(char));
+	arr = malloc(size * sizeof(char));
 
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
-	unsigned int i;
-
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
