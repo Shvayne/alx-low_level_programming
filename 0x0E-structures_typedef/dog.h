b6010d84a9d54fb07c196bf3c7dbfa670fe3e704
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-20 15:03:41 -0700

    added documentation to header file to improve reability

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_G
 
+/**
+ * struct dog - sets name, age and owner
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: this header file defines a struct
+ * with 3 members
+ */
 struct dog
 {
 	char *name;
