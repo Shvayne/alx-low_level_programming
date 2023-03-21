2135eae38c1c05714df9dc088b535443a284b66e
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-21 07:44:53 -0700

    removed strcpy function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,6 @@
 /**
  * init_dog - uses struct to access dog owner
  * parameter
- * struct dog - identifies the dog owner's parameters
  * @d: variable
  * @name: first member
  * @age: second member
@@ -15,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
