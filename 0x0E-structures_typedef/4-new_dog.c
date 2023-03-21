75d04ddb7b551d4ee6e6ad585df1efdc9681994f
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-21 10:13:56 -0700

    fixed function to allocate judt enough size for the copied strings

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "dog.h"
 /**
@@ -11,12 +11,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int length1, length2;
 
 	new_dog = malloc(sizeof(dog_t));
+	length1 = strlen(name);
+	length2 = strlen(owner);
 
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
-	new_dog->owner = owner;
+	new_dog->name = malloc(sizeof(char) * (length1 + 1));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc(sizeof(char) * (length2 + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		free(new_dog->name);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
+	new_dog->age = age;
 	return (new_dog);
 }
