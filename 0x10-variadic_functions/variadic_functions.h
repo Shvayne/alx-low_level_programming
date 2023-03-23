e4bd9cf24dfa97eb199aaa8fd7928fe01900603a
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-23 12:56:08 -0700

    added header, function to add inifite num

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VAR_H
+#define VAR_H
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
