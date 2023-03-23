87e4947941968abdd36ecf92b19a5554922f5156
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-23 12:36:34 -0700

    added header, function to add inifite num

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * sum_them_all - sums all parameters passed to the func
+ * @n: number of parameters
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+	va_list args;
+
+	va_start(args, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < n; i++)
+		sum = sum + va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+here we learn all about variadic functions and its usefulness and implementations in function like printf and scanf
