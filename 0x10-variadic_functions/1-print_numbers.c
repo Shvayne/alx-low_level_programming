7a56fbfcd86ea3a3395bc355798e7105411924bb
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-23 14:27:45 -0700

    added function to print parameters

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,22 +3,22 @@
 #include "variadic_functions.h"
 /**
  * print_numbers - print int
- * @seperator: constant seperator
- * @n: number of parameters
+ * @separator: constant seperator
+ * @n: number of parameters passed
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	int num = va_arg(args, unsigned int);
 	va_list args;
+	int num = va_arg(args, unsigned int);
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
-		printf("%d", sum);
+		printf("%d", num);
 	while (separator)
 	{
 		if (i != n - 1)
-			printf("%s", seperator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(args);
