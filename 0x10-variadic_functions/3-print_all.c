eda4abb9bc734d791faae784628c47b7fc7a7fb4
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-24 04:17:41 -0700

    added format specifier function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,61 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints all arguments, irrespective of type and number
+ * @format: format specifier
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char c;
+	int x;
+	float f;
+	char *s;
+	int flag = 0;
+
+	va_start(args, format);
+	while (format && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				flag = 1;
+				break;
+			case 'i':
+				x = va_arg(args, int);
+				printf("%d", x);
+				flag = 1;
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				printf("%f", f);
+				flag = 1;
+				break;
+			case 's':
+				s = va_arg(args, char*);
+				if (s == NULL)
+				{
+					printf("(nil)");
+				}
+				else
+				{
+					printf("%s", s);
+				}
+				flag = 1;
+				break;
+			default:
+				flag = 0;
+				break;
+		}
+		if (format[i + 1] != '\0' && flag == 1)
+			printf(", ");
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
