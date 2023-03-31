76c2498e4472e5a46bd8b0c9de741f508cef9692
Christiana Apinoko <christyapinoko@gmail.com>
2023-03-31 08:54:19 -0700

    added function to print list elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_list - prints the element in a list
+ * @h: head of list
+ * Return: count of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+
+	while (h != NULL)
+	{
+		printf("[%d] ", h->len);
+		printf("%s\n", h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
