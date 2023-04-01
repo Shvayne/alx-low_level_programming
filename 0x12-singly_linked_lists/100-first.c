19ed247acdfc2837a351d9fd9e94e741501f73dc
Christiana Apinoko <christyapinoko@gmail.com>
2023-04-01 08:45:09 -0700

    added function to print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_before_main - executes a function to print some text
+ * before the main function is executed
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI"
+			"bore my house upon my back!\n");
+}
