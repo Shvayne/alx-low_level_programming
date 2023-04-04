88281c345f34c811dd6b4435f46af5a447c7cfa0
Christiana Apinoko <christyapinoko@gmail.com>
2023-04-03 22:29:35 -0700

    added function to search and return node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,9 +15,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		current = current->next;
 	}
-	if (current == NULL)
+	if (i == index && current != NULL)
+	{
+		return (current);
+	}
+	else
 	{
 		return (NULL);
 	}
-	return (current);
 }
