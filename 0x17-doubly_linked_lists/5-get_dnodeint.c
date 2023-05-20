8eeb38b197116b13eb1b8df1509f0f2b6408ba8c
christiana apinoko <christyapinoko@gmail.com>
2023-05-20 00:47:17 -0700

    add func to return node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - gets node at index specified
+ * @head: head of list
+ * @index: index to compare
+ * Return: Null
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+		current = current->next;
+		count++;
+	}
+	return (NULL);
+}
