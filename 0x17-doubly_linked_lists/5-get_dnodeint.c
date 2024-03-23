705b7eca5afeb85ef5f452b74592ec0fd8c89765
Christiana Apinoko <christyapinoko@gmail.com>
2024-03-23 06:43:23 -0700

    fixed betty warnings

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current = head;
-	int i = 0;
+	unsigned int i = 0;
 
 	while (current != NULL && i < index)
 	{
