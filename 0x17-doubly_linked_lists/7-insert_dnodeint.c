b8913419be27a3b93c20f718833212449f34c057
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-03-23 13:51:53 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,13 +17,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
     if(*h == NULL || idx == 0)
     {
+        new_node->n = n;
+        new_node->prev = NULL;
         new_node->next = *h;
         if (*h != NULL)
         {
             (*h)->prev = new_node;
         }
         *h = new_node;
-        return;
+        return (new_node);
     }
 
     while (current != NULL && i < idx)
@@ -37,16 +39,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
         {
             *h = (*h)->next;
         }
-        (*h)->next = new_node;
-        new_node->prev = *h;
         new_node->n = n;
-        return;
+        new_node->prev = *h;
+        new_node->next = NULL;
+        (*h)->next = new_node;
+        return (new_node);
     }
+    new_node->n = n;
+    new_node->prev = current;
     new_node->next = current->next;
     if (current->next != NULL)
     {
         current->next->prev = new_node;
-        new_node->prev = current;
-        new_node->n = n;
     }
+    current->next = new_node;
+    return (new_node);
 }
