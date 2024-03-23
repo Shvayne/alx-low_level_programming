313649955d9462dc6cd92b0ca60baa441158dfe3
Christiana Apinoko <christyapinoko@gmail.com>
2024-03-23 06:25:13 -0700

    fixed betty warnings

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,26 +9,26 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *temp = malloc(sizeof(dlistint_t)); //create a new node
-    dlistint_t *current = *head; //create a current pointer and make it start from the beginning
+	dlistint_t *temp = malloc(sizeof(dlistint_t));
+	dlistint_t *current = *head;
 
-    if (temp == NULL) //if malloc fails
-    {
-        return (NULL);
-    } 
-    temp->n = n; 
-    temp->next = NULL; //make sure temp.next is pointing at NULL
-    if (*head == NULL) //if the list is empty
-    {
-        *head = temp; //make temp the head
-        temp->prev = NULL; //make its previous pointer also NuLL
-        return (temp);
-    }
-    while(current->next != NULL) //traversing the list
-    {
-        current = current->next;
-    }
-    current->next = temp; //when it gets to the null, make its next pointer point to the newnode, connecting them
-    temp->prev = current; //make temp previous pointer point to current
-    return (temp);
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->n = n;
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+		temp->prev = NULL;
+		return (temp);
+	}
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = temp;
+	temp->prev = current;
+	return (temp);
 }
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,17 +9,17 @@
 
 int sum_dlistint(dlistint_t *head)
 {
-    if (head == NULL)
-    {
-        return (0);
-    }
-    dlistint_t *current = head;
-    int sum = 0;
+	if (head == NULL)
+	{
+		return (0);
+	}
+	dlistint_t *current = head;
+	int sum = 0;
 
-    while (current != NULL)
-    {
-        sum += current->n;
-        current = current->next;
-    }
-    return (sum);
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
 }
