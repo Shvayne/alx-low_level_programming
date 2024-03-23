f4f0e067b030f518e63ecaf33393aaf341320a98
Christiana Apinoko <christyapinoko@gmail.com>
2024-03-22 18:25:12 -0700

    fixed betty warnings

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,36 +10,12 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while(h != NULL)
-    {
-        len++;
-        h= h->next;
-    }
-    return (len);
-}
-int main(void)
-{
-    dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        printf("Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = dlistint_len(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (EXIT_SUCCESS);
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
 }
