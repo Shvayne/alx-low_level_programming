d13885141f4acc960df210e31110c00456a43472
Christiana Apinoko <christyapinoko@gmail.com>
2024-07-17 09:34:28 -0700

    fix betty errors

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 #include "search_algos.h"
 
 /**
@@ -11,32 +10,33 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t step = sqrt(size); /* Calculate the jump step size as the square root of the array size */
-    size_t prev = 0; /* Initialize the previous index to 0 */
-    size_t i; /* Loop variable for linear search */
-    if (array == NULL || size == 0)
-    {
-        return (-1);
-    }
+	size_t step = sqrt(size); /* Calculate the jump step*/
+	size_t prev = 0;
+	size_t i; /* Loop variable for linear search */
 
-    /* Jump through the array in steps */
-    while (prev < size && array[prev] < value)
-    {
-        /* Print the current index being checked */
-        printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
-        prev += step; /* Move to the next jump block */
-    }
-    /*print indexes being searched*/
-    printf("Value found between index [%lu] and [%lu]\n", prev - step, prev);
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
 
-    /*perform linear search within block*/
-    for (i = prev - step; i < size && i <= prev; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-        {
-            return (i);
-        }
-    }
-    return (-1);
+	/* Jump through the array in steps */
+	while (prev < size && array[prev] < value)
+	{
+		/* Print the current index being checked */
+		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
+		prev += step; /* Move to the next jump block */
+	}
+	/*print indexes being searched*/
+	printf("Value found between index [%lu] and [%lu]\n", prev - step, prev);
+
+	/*perform linear search within block*/
+	for (i = prev - step; i < size && i <= prev; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,47 +10,47 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t low = 0;
-    size_t high = size - 1;
-    size_t pos;
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t pos;
 
-    if (array == NULL || size == 0)
-    {
-        return (-1);
-    }
-    while (low <= high && value >= array[low] && value <= array[high])
-    {
-        if (low == high)
-        {
-            if (array[low] == value)
-            {
-                return (low);
-            }
-            return (-1);
-        }
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		if (low == high)
+		{
+			if (array[low] == value)
+			{
+				return (low);
+			}
+			return (-1);
+		}
 
-        pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 
-        if (pos >= size)
-        {
-            printf("Value checked array[%lu] is out of range\n", pos);
-            return (-1);
-        }
+		if (pos >= size)
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			return (-1);
+		}
 
-        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-        if (array[pos] == value)
-        {
-            return (pos);
-        }
-        if (array[pos] < value)
-        {
-            low = pos + 1;
-        }
-        else
-        {
-            high = pos - 1;
-        }
-    }
-    return (-1);
+		if (array[pos] == value)
+		{
+			return (pos);
+		}
+		if (array[pos] < value)
+		{
+			low = pos + 1;
+		}
+		else
+		{
+			high = pos - 1;
+		}
+	}
+	return (-1);
 }
